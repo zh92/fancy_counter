226e67e89acd581fba6ee234159508403adf52a1
zh92 <zhehao92@gmail.com>
2023-12-10 22:45:43 +0800

    Getting started

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+function App() {
+  return (
+  <main>
+    <div>
+      <h1>Fancy Counter</h1>
+      <p>0</p>
+      <button>Reset</button>
+    </div>
+    <div>
+      <button>-</button>
+      <button>+</button>
+    </div>
+  </main>
+  );
+}
+
+export default App
